96953cd721f073aa6fed50a79eb4531816fd96f3
Todd D. Vance <tdvance@gmail.com>
2016-07-29 12:12:21 -0400

    Functional start, quit, and back buttons.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void PauseLevel()
+    {
+        Debug.Log("Pause requested");
+        LoadLevel("Start");
+    }
+}
